36e13464621c85578ee4cddbd583925a501bd460
BoletaScociis <99562369+BoletaScociis@users.noreply.github.com>
2022-05-01 19:27:51 +0100

    Add files via upload

---
+++ config.h
@@ -0,0 +1,5 @@
+// Obins stock firmware has something similar to this already enabled, but disabled by default in QMK
+#define TAPPING_TERM 160
+#define AUTO_SHIFT_TIMEOUT 160
+#define AUTO_SHIFT_MODIFIERS
+#define RETRO_SHIFT 480
