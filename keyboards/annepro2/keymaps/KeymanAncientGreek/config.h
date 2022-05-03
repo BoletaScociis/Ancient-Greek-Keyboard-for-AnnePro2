7e6cba5f369cb319a9b3b75a6ef2059fe528bc3a
BoletaScociis <99562369+BoletaScociis@users.noreply.github.com>
2022-05-03 08:59:44 +0100

    Add files via upload

--- keyboards/annepro2/keymaps/KeymanAncientGreek/config.h
+++ keyboards/annepro2/keymaps/KeymanAncientGreek/config.h
@@ -1,3 +1,5 @@
 // Obins stock firmware has something similar to this already enabled, but disabled by default in QMK
-#define PERMISSIVE_HOLD
-#define TAPPING_TERM 175
+#define TAPPING_TERM 160
+#define AUTO_SHIFT_TIMEOUT 160
+#define AUTO_SHIFT_MODIFIERS
+#define RETRO_SHIFT 480
